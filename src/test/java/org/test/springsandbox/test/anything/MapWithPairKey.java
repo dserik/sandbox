e2ce62fbc6e643879a06df041ee43e3b96daf024
Daryn Serikbayev <daryn.serikbayev@gmail.com>
2019-10-09 09:43:20 +0600

    map with pair key

---
+++ src/test/java/org/test/springsandbox/test/anything/MapWithPairKey.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 - 2018 ICORE Software Development LLP
+ * http://icode.kz
+ */
+package org.test.springsandbox.test.anything;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.data.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapWithPairKey {
+
+    public enum First {
+        A, B, C
+    }
+
+    public enum Second {
+        X, Y, Z
+    }
+
+    @Test
+    public void testPairKey() {
+        Pair<First, Second> one = Pair.of(First.A, Second.Z);
+        Pair<First, Second> two = Pair.of(First.B, Second.Z);
+        Pair<First, Second> three = Pair.of(First.A, Second.Z);
+
+        Assert.assertNotEquals(one, two);
+        Assert.assertEquals(one, three);
+
+        Map<Pair<First, Second>, String> pairKeyedMap = new HashMap<>();
+        pairKeyedMap.put(one, "one");
+        pairKeyedMap.put(two, "two");
+
+        // get with other similar(three) key
+        Assert.assertEquals("one", pairKeyedMap.get(three));
+    }
+}
