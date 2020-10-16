4345427ea495f0854942de2f66711022da738cd0
dserikbayev@beeline.kz <daIlsdf#2^%gffdlLL>
2020-10-16 11:47:37 +0600

    changes to test branch

--- src/main/java/org/test/springsandbox/service/DownloadService.java
+++ src/main/java/org/test/springsandbox/service/DownloadService.java
@@ -15,6 +15,8 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class DownloadService {
 
+    //changes to test branch
+
     private static final String DOWNLOAD_DIR =
             System.getProperty("user.dir") + File.separator +
                     "download" + File.separator;
